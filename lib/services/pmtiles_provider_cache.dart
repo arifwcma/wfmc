592f7f0d9367a9fcecbf984b4f9839d969317003
arifwcma <arif.rahman@wcma.vic.gov.au>
2026-05-04 15:55:05 +1000

    done

--- lib/services/pmtiles_provider_cache.dart
+++ lib/services/pmtiles_provider_cache.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:vector_map_tiles_pmtiles/vector_map_tiles_pmtiles.dart';
 
 class PmTilesProviderCache {
   final Map<String, Future<PmTilesVectorTileProvider>> _providersByUrl = {};
 
   Future<PmTilesVectorTileProvider> provider(String url) {
-    return _providersByUrl.putIfAbsent(
-      url,
-      () => PmTilesVectorTileProvider.fromSource(url),
-    );
+    return _providersByUrl.putIfAbsent(url, () => _load(url));
+  }
+
+  Future<PmTilesVectorTileProvider> _load(String url) async {
+    try {
+      return await PmTilesVectorTileProvider.fromSource(url);
+    } catch (e, st) {
+      debugPrint('PmTilesProviderCache: failed to load $url: $e\n$st');
+      _providersByUrl.remove(url);
+      rethrow;
+    }
   }
 }
